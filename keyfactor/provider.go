3684faa41e258fdda9b1db7c9983210da10929c2
spbsoluble <1661003+spbsoluble@users.noreply.github.com>
2025-01-16 13:35:24 -0800

    chore: Bump version

--- keyfactor/provider.go
+++ keyfactor/provider.go
@@ -31,7 +31,7 @@ const (
 	EnvVarUsage              = "This can also be set via the `%s` environment variable."
 	DefaultValMsg            = "Default value is `%v`."
 	InvalidProviderConfigErr = "invalid provider configuration"
-	Version                  = "2.2.0-rc.15"
+	Version                  = "2.2.0-rc.16"
 )
 
 var (
